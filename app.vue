0dddd3b8df29769004f5484eff63b42e875a0178
emodot <emayeodavid@gmail.com>
2024-04-30 12:40:24 +0100

    fix: fixed gsap issue

--- app.vue
+++ app.vue
@@ -5,8 +5,8 @@
 </template>
 
 <script setup>
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+// import gsap from "gsap";
+// import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-gsap.registerPlugin(ScrollTrigger);
+// gsap.registerPlugin(ScrollTrigger);
 </script>
